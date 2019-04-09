fb2a305aa3c83b13361cbad3c75a7c9962b7ee6d
seanchok <seanchok@gmail.com>
2019-04-10 04:19:26 +0800

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7f021383a0d6ab477773.js"
+    "url": "webpack-runtime-719e8faac38cd3cb4cbc.js"
   },
   {
     "url": "app-905d0768cca26d19e62a.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ed124ea07c6ee321b16b382ba57f54ca"
+    "revision": "3425cbebe2f956fdba2377bf1299c534"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
