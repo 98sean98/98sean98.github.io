9d36d3d88782ee761809fb638e6be2ebeb732649
seanchok <seanchok@gmail.com>
2019-09-28 16:39:46 +0800

    build App.js using styled components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import theme from './design/theme';
+import mars from './images/mars.jpg';
+import siteConfig from './siteConfig';
+
+import Profile from './components/profile';
 
 class App extends Component {
   render() {
     const AppBody = styled.div`
-      height: 100vh
+      @import url('https://fonts.googleapis.com/css?family=Noto+Sans&display=swap');
+      height: 96vh;
+      padding: 2vh 2vw;
       background-color: ${theme.backgroundColor};
-      font-size: calc(12px + 1vw);
+      background-image: url(${mars});
+      background-size: cover;
+      background-repeat: no-repeat;
+      background-position: center;
+      text-align: center;
+      font-size: calc(12px + 2vmax);
       color: ${theme.textColor};
+      font-family: 'Noto Sans', sans-serif;
     `;
+
     return (
-      <ThemeProvider theme={{ mode: 'dark', color: 'primary' }}>
-        <AppBody>Todo</AppBody>
+      <ThemeProvider theme={{ mode: 'dark', color: 'primary', fontFamily: 'default' }}>
+        <AppBody>profile</AppBody>
       </ThemeProvider>
     );
   }
