35feda0ca2af8e53f04fbc662b479fedeeda9353
seanchok <seanchok@gmail.com>
2019-09-29 02:58:49 +0800

    Updates

---
+++ precache-manifest.374e02e0c0a051c7b31098cd1c5c0425.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52601b627cb76009e77150b314de321c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6bebd86530a73a739a7",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7530996bafb2cc40ce18",
+    "url": "/static/js/2.db7b5706.chunk.js"
+  },
+  {
+    "revision": "a6bebd86530a73a739a7",
+    "url": "/static/js/main.80d2430b.chunk.js"
+  },
+  {
+    "revision": "e72921bdbf90c087d29f",
+    "url": "/static/js/runtime-main.4f337f23.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
