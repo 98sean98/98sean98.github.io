ce3bd6f52f3198b671c8bd58b751fff0b3404f93
Sean Chok <seanchok@gmail.com>
2019-09-30 01:56:46 +0800

    minor changes (#6)
    * update robomaster link
    
    * add hoverable to subsection entries

--- src/components/entry.js
+++ src/components/entry.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import theme from '../design/theme';
+import posed from 'react-pose';
+import color from '../design/color';
 
 const styles = {
   text: {
     margin: '0.5vh auto',
   },
 };
+
+const Hoverable = posed.div({
+  hoverable: true,
+  init: {
+    background: color.primary,
+  },
+  hover: {
+    background: color.secondary,
+  },
+});
 
 class Entry extends Component {
   render() {
@@ -18,12 +30,14 @@ class Entry extends Component {
     `;
 
     return (
-      <Container href={entry.link} target="_blank" rel="noopener noreferrer">
-        <h3 style={styles.text}>{entry.head}</h3>
-        <h4 style={styles.text}>{entry.subhead}</h4>
-        <h5 style={styles.text}>{entry.period}</h5>
-        <p style={styles.text}>{entry.description}</p>
-      </Container>
+      <Hoverable style={{ borderRadius: '15px' }}>
+        <Container href={entry.link} target="_blank" rel="noopener noreferrer">
+          <h3 style={styles.text}>{entry.head}</h3>
+          <h4 style={styles.text}>{entry.subhead}</h4>
+          <h5 style={styles.text}>{entry.period}</h5>
+          <p style={styles.text}>{entry.description}</p>
+        </Container>
+      </Hoverable>
     );
   }
 }
