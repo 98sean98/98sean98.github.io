9e10a83e7ebbb44c0f1aa3b27c3125dcc8023e8d
Sean Chok <seanchok@gmail.com>
2019-10-10 01:12:48 +0800

    minor updates (#7)
    * remove profile tags and intro phrase
    
    * fix period error in idtech camps info
    
    * update readme: bad designer disclaimer, gh-pages deploy method, downloadable resume update method
    
    * add website link to readme

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # My Website Build
 
+[Click](https://98sean98.github.io)
+
 This is a rebuild of the original react/gatsy/graphql website. I decided to redo the frontend and simplify how the whole thing works. To be honest, maintaining the old one was not exactly easy because I did not fully understand the dependencies used. I was not the designer of the core components.
 
 I decided to strive on designing a good looking website.
+
+_Full disclaimer: I'm basically the worst designer you can find, at least my friends told me so._
 
 ### To Develop
 
@@ -14,4 +18,8 @@ I decided to strive on designing a good looking website.
 
 ### To Deploy
 
-To do
+`npm run deploy`
+
+### To update downloadable resume
+
+Update the `src/resume.pdf` file.
--- src/components/profile.js
+++ src/components/profile.js
@@ -26,26 +26,6 @@ const IconBox = posed.div({
 });
 
 class Profile extends Component {
-  state = {
-    tag: this.props.profile.tags[0][0],
-  };
-
-  componentDidMount() {
-    const delay = 150;
-    const tags = this.props.profile.tags;
-    let i = 0,
-      j = 0;
-    setInterval(() => {
-      j += 1;
-      if (j === tags[i].length) {
-        i = (i + 1) % tags.length;
-        j = 0;
-      }
-      let newTag = tags[i].slice(0, j + 1);
-      this.setState({ tag: newTag });
-    }, delay);
-  }
-
   generateIconBox = (link, icon) => {
     const IconLink = styled.a`
       text-decoration: none;
@@ -77,20 +57,16 @@ class Profile extends Component {
     const TextContainer = styled.div`
       background: ${theme.backgroundColor};
       border-radius: 10px;
-      margin: auto 2vw;
-      margin-top: 8vh;
+      max-width: calc(500px + 2vw);
+      margin: auto;
+      margin-top: 10vh;
       padding: 1vmin;
       font-size: calc(20px + 2vmax);
     `;
 
-    const Tag = styled.div`
-      font-size: 0.5em;
-      color: ${theme.color};
-    `;
-
     const Description = styled.div`
-      font-size: 0.4em;
-      margin: 3vh 2vw;
+      font-size: 0.35em;
+      margin: 2vh 2vw;
     `;
 
     const IconContainer = styled.div`
@@ -108,14 +84,12 @@ class Profile extends Component {
     `;
 
     const { profile } = this.props;
-    const { tag } = this.state;
 
     return (
       <ProfileBody>
         <TextContainer>
-          <div>Hi | I'm </div>
           <div>{profile.myName}</div>
-          <Tag>{tag}</Tag>
+
           <Description>{profile.description}</Description>
         </TextContainer>
 
