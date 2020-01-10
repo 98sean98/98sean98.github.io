cf6ce1a6fc268a769093270d031913abb0c8d0fc
seanchok <seanchok@gmail.com>
2020-01-11 00:01:00 +0800

    resturcture siteConfig
    #13

--- src/siteConfig.js
+++ src/siteConfig.js
@@ -1,128 +1,125 @@
-export const siteConfig = {
-  profile: {
-    myName: 'Sean Chok',
-    email: 'mailto:seanchok@connect.hku.hk',
-    linkedIn: 'https://www.linkedin.com/in/sean-chok-9035b2144/',
-    github: 'https://github.com/98sean98',
-    tags: ['mechanical engineer', 'space enthusiast', 'robot builder', 'web developer'],
-  },
-  contents: {
-    subsections: [
+export const profile = {
+  myName: 'Sean Chok',
+  email: 'mailto:seanchok@connect.hku.hk',
+  linkedIn: 'https://www.linkedin.com/in/sean-chok-9035b2144/',
+  github: 'https://github.com/98sean98',
+  tags: ['mechanical engineer', 'space enthusiast', 'robot builder', 'web developer'],
+};
+
+export const contents = [
+  {
+    tag: 'EDUCATION',
+    entries: [
       {
-        tag: 'EDUCATION',
-        entries: [
-          {
-            head: 'The University of Hong Kong',
-            description: 'Bachelor of Engineering (Mechanical)',
-            period: '09/2018 - 06/2022',
-            logo: 'HKULogo.png',
-            link: 'http://me.hku.hk/',
-          },
-        ],
-      },
-      {
-        tag: 'WORKING EXPERIENCE',
-        entries: [
-          {
-            head: 'BrainBuilder',
-            subhead: 'Teacher / Curriculum Writer',
-            period: '11/2016 - 08/2018',
-            description:
-              'My role included tailoring our Math program to suit the best interests of individual students, providing feedback to parents, etc.',
-            logo: 'BrainBuilder-logo.png',
-            link: 'http://brainbuildermaths.com/',
-          },
-          {
-            head: 'iDTech Summer Camps',
-            subhead: 'Instructor',
-            period: 'Summer 2019',
-            description:
-              "I taught children in STEM education during week-long summer camps; particularly, my class explores the world of Minecraft from a world builder's point of view. We also make sure that campers enjoy making friends, and having fun here in a safe environment.",
-            logo: 'idtech-logo.png',
-            link: 'https://www.idtech.com/',
-          },
-        ],
-      },
-      {
-        tag: 'PROJECTS AND OTHER EXPERIENCE',
-        entries: [
-          {
-            head: 'Robomaster Team Herkules',
-            subhead: 'Mechanical Group Member - Drone & Hero Robots',
-            description:
-              'HKU team for DJI Robomaster Robotics Competition 2018/19. I was in-charged of designing, building, and testing 2 robots.',
-            logo: 'Herkules.png',
-            link: 'http://innowing.hk/robomaster2019/',
-          },
-          {
-            head: 'Formula Student - HKU Racing',
-            subhead: 'Aerodynamics Division',
-            description:
-              'HKU team for Formula Student UK 2019/2020. I am in-charged of performing computational fluid dynamics analysis to optimize wings and undertray design.',
-            logo: 'hkur.jpg',
-            link: 'http://innowing.hk/hkur/',
-          },
-        ],
-      },
-      {
-        tag: 'LEADERSHIP ROLES',
-        entries: [
-          {
-            head: 'RC Tech',
-            subhead: 'Co-founder',
-            period: '10/2018 - Present',
-            description:
-              'RC Tech is a student-led community in R.C. Lee Hall (HKU) that empowers innovation, and implementation of technology through multiple projects. The club holds workshops on topics ranging from web development, electronics and IoT hardware, to hydroponics. Club members also publish articles related to similar topics.',
-            logo: 'RcTech-logo.png',
-            link: 'https://www.rctech.club/',
-          },
-        ],
-      },
-      {
-        tag: 'ACHIEVEMENTS AND AWARDS',
-        entries: [
-          {
-            head: 'Tech Crunch Hackathon 2018, Hong Kong',
-            subhead: '2nd Runner Up',
-            description:
-              'Developed BlockBank, a blockchain powered e-wallet for easy and secure bank transactions.',
-            logo: 'TechCrunch-Logo.png',
-            link: null,
-          },
-          {
-            head: 'Global Legal Hackathon 2019, Hong Kong',
-            subhead: '1st Runner Up',
-            description:
-              'Developed CLAIMate, an app for petty claims featuring the court of public opinion.',
-            logo: 'GLH2019-logo.png',
-            link: 'https://globallegalhackathon.com/',
-          },
-          {
-            head: 'Fintech Week Hackathon 2019',
-            subhead: '1st Runner Up',
-            description:
-              'Developed Navis, an API solution coupled with a user platform as an escalation service for Hong Kong Export Credit Insurance Corporation.',
-            logo: 'fintechweek-hackathon-logo.png',
-            link: 'https://navis-app.co',
-          },
-          {
-            head: 'HKIMUNC 2019',
-            subhead: 'ECOFIN council - Outstanding Delegate',
-            description:
-              "Delegated as the People's Republic of China, and discussed about the effects of protectionist policies on international trade.",
-            logo: 'HKIMUNC_2019_logo.png',
-            link: null,
-          },
-          {
-            head: 'Student/Staff Project Fund',
-            subhead: 'Project fund of HK$5000',
-            description:
-              'Student/Staff Project fund by R.C. Lee Hall at The University of Hong Kong for RC Tech.',
-            logo: 'RcTech-logo.png',
-            link: 'https://www.rctech.club/',
-          },
-        ],
+        head: 'The University of Hong Kong',
+        description: 'Bachelor of Engineering (Mechanical)',
+        period: '09/2018 - 06/2022',
+        image: 'HKULogo.png',
+        link: 'http://me.hku.hk/',
       },
     ],
   },
-};
+  {
+    tag: 'WORKING EXPERIENCE',
+    entries: [
+      {
+        head: 'BrainBuilder',
+        subhead: 'Teacher / Curriculum Writer',
+        period: '11/2016 - 08/2018',
+        description:
+          'My role included tailoring our Math program to suit the best interests of individual students, providing feedback to parents, etc.',
+        image: 'BrainBuilder-logo.png',
+        link: 'http://brainbuildermaths.com/',
+      },
+      {
+        head: 'iDTech Summer Camps',
+        subhead: 'Instructor',
+        period: 'Summer 2019',
+        description:
+          "I taught children in STEM education during week-long summer camps; particularly, my class explores the world of Minecraft from a world builder's point of view. We also make sure that campers enjoy making friends, and having fun here in a safe environment.",
+        image: 'idtech-logo.png',
+        link: 'https://www.idtech.com/',
+      },
+    ],
+  },
+  {
+    tag: 'PROJECTS AND OTHER EXPERIENCE',
+    entries: [
+      {
+        head: 'Robomaster Team Herkules',
+        subhead: 'Mechanical Group Member - Drone & Hero Robots',
+        description:
+          'HKU team for DJI Robomaster Robotics Competition 2018/19. I was in-charged of designing, building, and testing 2 robots.',
+        image: 'Herkules.png',
+        link: 'http://innowing.hk/robomaster2019/',
+      },
+      {
+        head: 'Formula Student - HKU Racing',
+        subhead: 'Aerodynamics Division',
+        description:
+          'HKU team for Formula Student UK 2019/2020. I am in-charged of performing computational fluid dynamics analysis to optimize wings and undertray design.',
+        image: 'hkur.jpg',
+        link: 'http://innowing.hk/hkur/',
+      },
+    ],
+  },
+  {
+    tag: 'LEADERSHIP ROLES',
+    entries: [
+      {
+        head: 'RC Tech',
+        subhead: 'Co-founder',
+        period: '10/2018 - Present',
+        description:
+          'RC Tech is a student-led community in R.C. Lee Hall (HKU) that empowers innovation, and implementation of technology through multiple projects. The club holds workshops on topics ranging from web development, electronics and IoT hardware, to hydroponics. Club members also publish articles related to similar topics.',
+        image: 'RcTech-logo.png',
+        link: 'https://www.rctech.club/',
+      },
+    ],
+  },
+  {
+    tag: 'ACHIEVEMENTS AND AWARDS',
+    entries: [
+      {
+        head: 'Tech Crunch Hackathon 2018, Hong Kong',
+        subhead: '2nd Runner Up',
+        description:
+          'Developed BlockBank, a blockchain powered e-wallet for easy and secure bank transactions.',
+        image: 'TechCrunch-Logo.png',
+        link: null,
+      },
+      {
+        head: 'Global Legal Hackathon 2019, Hong Kong',
+        subhead: '1st Runner Up',
+        description:
+          'Developed CLAIMate, an app for petty claims featuring the court of public opinion.',
+        image: 'GLH2019-logo.png',
+        link: 'https://globallegalhackathon.com/',
+      },
+      {
+        head: 'Fintech Week Hackathon 2019',
+        subhead: '1st Runner Up',
+        description:
+          'Developed Navis, an API solution coupled with a user platform as an escalation service for Hong Kong Export Credit Insurance Corporation.',
+        image: 'fintechweek-hackathon-logo.png',
+        link: 'https://navis-app.co',
+      },
+      {
+        head: 'HKIMUNC 2019',
+        subhead: 'ECOFIN council - Outstanding Delegate',
+        description:
+          "Delegated as the People's Republic of China, and discussed about the effects of protectionist policies on international trade.",
+        image: 'HKIMUNC_2019_logo.png',
+        link: null,
+      },
+      {
+        head: 'Student/Staff Project Fund',
+        subhead: 'Project fund of HK$5000',
+        description:
+          'Student/Staff Project fund by R.C. Lee Hall at The University of Hong Kong for RC Tech.',
+        image: 'RcTech-logo.png',
+        link: 'https://www.rctech.club/',
+      },
+    ],
+  },
+];
