cbca9f31b7ceae4c693b07d7cbd5879075e9181a
seanchok <seanchok@gmail.com>
2020-01-11 11:35:02 +0800

    adjust grid layout to center cards horizontally and vertically

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -25,7 +25,7 @@ const Resume = () => {
   return (
     <>
       <div className={classes.root}>
-        <Grid container spacing={4}>
+        <Grid container spacing={4} justify="space-between" alignItems="center">
           {entries.map(entry => (
             <Grid item xs={12} sm={6} md={4} key={entry.head}>
               <Card entry={entry} image={images[entry.image]} />
