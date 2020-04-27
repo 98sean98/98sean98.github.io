2e0aa186c6058283957166736c19e35ee5ec2b42
seanchok <seanchok@gmail.com>
2020-04-27 21:51:48 +0100

    add icon buttons as links

--- src/components/landing/Profile.js
+++ src/components/landing/Profile.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Typography from '@material-ui/core/Typography';
-import { profile } from '../../siteConfig';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import EmailIcon from '@material-ui/icons/Email';
 import Zoom from 'react-reveal/Zoom';
 import Fade from 'react-reveal/Fade';
-
-const delay = 500;
+import { profile } from '../../siteConfig';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -13,6 +16,9 @@ const useStyles = makeStyles(theme => ({
   },
   textBox: {
     margin: theme.spacing(2),
+  },
+  iconGrid: {
+    marginLeft: theme.spacing(2),
   },
 }));
 
@@ -30,6 +36,17 @@ export const Profile = () => {
     <Typography variant={'body1'}>I study at {school}</Typography>,
   ];
 
+  const icons = [
+    { link: profile.github, component: <GitHubIcon /> },
+    { link: profile.linkedIn, component: <LinkedInIcon /> },
+    { link: profile.email, component: <EmailIcon /> },
+  ];
+
+  const delay = 500;
+  const secondGroupDelay = delay * (children.length + 1);
+  const thirdGroupDelay = secondGroupDelay + delay * 3;
+  const fourthGroupDelay = thirdGroupDelay + delay;
+
   return (
     <div className={classes.root}>
       {children.map((child, index) => (
@@ -40,10 +57,10 @@ export const Profile = () => {
         </div>
       ))}
       <div className={classes.textBox}>
-        <Zoom clear delay={delay * (children.length + 1)}>
+        <Zoom clear delay={secondGroupDelay}>
           <Typography variant={'body1'}>I excel as a </Typography>
         </Zoom>
-        <Fade cascade duration={600 * tags.length} delay={delay * (children.length + 2)}>
+        <Fade cascade duration={600 * tags.length} delay={secondGroupDelay + delay}>
           <ul>
             {tags.map((tag, index) => (
               <li key={index}>
@@ -53,6 +70,26 @@ export const Profile = () => {
           </ul>
         </Fade>
       </div>
+      <Zoom clear delay={thirdGroupDelay}>
+        <div className={classes.textBox}>
+          <Typography variant={'body1'}>Would you like to work with me? Contact me at </Typography>
+        </div>
+      </Zoom>
+      <Fade cascade bottom duration={500 * icons.length} delay={fourthGroupDelay}>
+        <Grid container spacing={2} className={classes.iconGrid}>
+          {icons.map((icon, index) => (
+            <Grid item key={index}>
+              <IconButton
+                color={'secondary'}
+                href={icon.link}
+                target={'_blank'}
+                rel={'noopener noreferrer'}>
+                {icon.component}
+              </IconButton>
+            </Grid>
+          ))}
+        </Grid>
+      </Fade>
     </div>
   );
 };
