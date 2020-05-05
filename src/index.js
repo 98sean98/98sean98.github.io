13af4bdd2aebc172161b3d81ac996fa648cabbb2
Sean Chok <seanchok@gmail.com>
2020-05-05 23:33:35 +0100

    add react-snap to pre-render html (#27)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { rootReducer } from './reducers';
+import { initialState, rootReducer } from './reducers';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import ThemeWrapper from './ThemeWrapper';
 
-const store = createStore(rootReducer);
+// Grab the state from a global variable injected into the server-generated HTML
+const preloadedState = window.__PRELOADED_STATE__;
+
+// Allow the passed state to be garbage-collected
+delete window.__PRELOADED_STATE__;
+
+// Create Redux store with initial state
+const store = createStore(rootReducer, preloadedState || initialState);
+
+// Tell react-snap how to save Redux state
+window.snapSaveState = () => ({
+  __PRELOADED_STATE__: store.getState(),
+});
 
 const Index = () => (
   <Provider store={store}>
@@ -18,7 +30,12 @@ const Index = () => (
   </Provider>
 );
 
-ReactDOM.render(<Index />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+if (rootElement.hasChildNodes()) {
+  hydrate(<Index />, rootElement);
+} else {
+  render(<Index />, rootElement);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
