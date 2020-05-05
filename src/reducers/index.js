db9236da9a418c23a5d765d38832d653c8785af2
seanchok <seanchok@gmail.com>
2020-05-05 23:30:37 +0100

    add react-snap to pre-render html

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,3 +2,7 @@ import { combineReducers } from 'redux';
 import { windowSize } from './windowSize';
 
 export const rootReducer = combineReducers({ windowSize });
+
+export const initialState = {
+  windowSize: { width: 1000, height: 1000 },
+};
