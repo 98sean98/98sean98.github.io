e7f56d4626fc542f06476e37d29c838c36a087e6
seanchok <seanchok@gmail.com>
2020-05-24 02:01:11 +0800

    build basic animations for rocket

--- src/components/projects/aerospace/Aerospace.js
+++ src/components/projects/aerospace/Aerospace.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Rocket } from './Rocket';
 import { SectionContainer } from '../../generic/SectionContainer';
+import Grid from '@material-ui/core/Grid';
+import { Details } from './Details';
 
 export const Aerospace = () => {
   return (
     <SectionContainer>
-      <Rocket />
+      <Grid container spacing={2} justify={'space-between'} alignItems={'center'}>
+        <Grid item xs={12} sm={4} md={3}>
+          <Rocket />
+        </Grid>
+        <Grid item>
+          <Details />
+        </Grid>
+      </Grid>
     </SectionContainer>
   );
 };
--- src/components/projects/aerospace/Rocket.js
+++ src/components/projects/aerospace/Rocket.js
@@ -1,22 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+import { Animate } from 'react-move';
+import { easePolyIn } from 'd3-ease';
 import { ReactComponent as RocketSVG } from './rocket.svg';
 
 const useStyles = makeStyles(theme => ({
   root: {},
   svg: {
     height: 150,
-    transform: 'rotate(90deg)',
   },
 }));
 
 export const Rocket = () => {
   const classes = useStyles();
+  const [isLaunched, setIsLaunched] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsLaunched(!isLaunched);
+  };
+
+  const start = () => ({
+    x: 0,
+    y: 0,
+    angle: 0,
+    opacity: 1,
+  });
+
+  const update = () => [
+    {
+      x: isLaunched ? [1000] : 0,
+      y: isLaunched ? [-500] : 0,
+      angle: isLaunched ? [45] : 0,
+      timing: { duration: 4000, ease: easePolyIn },
+    },
+    {
+      opacity: isLaunched ? 1 : [0, 1],
+      timing: { delay: 150, duration: 500 },
+    },
+  ];
 
   return (
     <Container className={classes.root}>
-      <RocketSVG className={classes.svg} />
+      <Animate start={start} update={update}>
+        {({ x, y, angle, opacity }) => (
+          <div
+            style={{
+              opacity,
+              position: 'relative',
+              WebkitTransform: `translate3d(${x}px, ${y}px, 0) rotate(${angle}deg)`,
+              transform: `translate3d(${x}px, ${y}px, 0) rotate(${angle}deg)`,
+            }}>
+            <RocketSVG className={classes.svg} />
+          </div>
+        )}
+      </Animate>
+      <Button onClick={handleButtonClick}>{isLaunched ? 'Reset' : 'Launch Me'}</Button>
     </Container>
   );
 };
