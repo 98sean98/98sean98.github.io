efc75656e6efec148febb5148a18833f740580c3
seanchok <seanchok@gmail.com>
2020-05-26 17:18:36 +0800

    move rocket details text into an expansion panel
    fix rocket root zIndex to have higher priority, and relative positioning

--- src/components/projects/aerospace/Details.js
+++ src/components/projects/aerospace/Details.js
@@ -5,6 +5,10 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import clsx from 'clsx';
 import rocket_calculations from '../../../pdf/rocket_calculations.pdf';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const useStyles = makeStyles(theme => ({
   alignCenter: {
@@ -15,6 +19,10 @@ const useStyles = makeStyles(theme => ({
   },
   section: {
     margin: `${theme.spacing(1)}px auto`,
+  },
+  expansionPanel: {
+    border: '1px solid rgba(0, 0, 0, .125)',
+    boxShadow: 'none',
   },
 }));
 
@@ -47,32 +55,44 @@ export const Details = ({ shouldAlignCenter }) => {
             </li>
           ))}
         </ul>
-        <Typography variant={'body1'} display={'block'} gutterBottom>
-          I was able to find solutions to the velocity after postulating a linear time function for
-          angular displacement. Then, the velocity function is used to find the 2 displacement
-          functions; and I stopped at relatively complicated definite integrals.
-        </Typography>
-        <Typography variant={'body1'} display={'block'} gutterBottom>
-          Looking at the integrals made me rethink my approach: even if the solutions were correct,
-          I did't think the interpolation algorithm used by the animation library to calculate the
-          x, y, and angle attributes of the rocket could handle such complicated computations within
-          milliseconds. According to the documentation of the library, interpolation should be
-          completed quickly because the same calculations need to be done for each finite increment
-          of time. Thus, I had to opt for an approximate solution using simple exponential and
-          trigonometric functions; and it wasn't too difficult to find what I needed; however, I had
-          to optimise the function for angular displacement to use an exponential function so that
-          the rocket behaves a little better.
-        </Typography>
-        <Typography variant={'body1'} display={'block'} gutterBottom>
-          After that, I programmed the approximated solution into the animation algorithm. The final
-          result is not too bad, but not accurate. Its worth noting that since a rocket's
-          acceleration increases over time as mass is lost due to burnt and ejected fuel, the
-          velocity increases in a somewhat exponential nature. I do plan to improve what I have
-          here, and I think one of the possible ways to use the initial analytical solution is to
-          first store numerical results in a table, then do a simple linear interpolation according
-          to what is needed.
-        </Typography>
-        <Typography variant={'body2'} display={'block'} gutterBottom>
+
+        <ExpansionPanel className={clsx(classes.section, classes.expansionPanel)}>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography variant={'body1'}>Read more</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <div>
+              <Typography variant={'body1'} gutterBottom>
+                I was able to find solutions to the velocity after postulating a linear time
+                function for angular displacement. Then, the velocity function is used to find the 2
+                displacement functions; and I stopped at relatively complicated definite integrals.
+              </Typography>
+              <Typography variant={'body1'} gutterBottom>
+                Looking at the integrals made me rethink my approach: even if the solutions were
+                correct, I did't think the interpolation algorithm used by the animation library to
+                calculate the x, y, and angle attributes of the rocket could handle such complicated
+                computations within milliseconds. According to the documentation of the library,
+                interpolation should be completed quickly because the same calculations need to be
+                done for each finite increment of time. Thus, I had to opt for an approximate
+                solution using simple exponential and trigonometric functions; and it wasn't too
+                difficult to find what I needed; however, I had to optimise the function for angular
+                displacement to use an exponential function so that the rocket behaves a little
+                better.
+              </Typography>
+              <Typography variant={'body1'} gutterBottom>
+                After that, I programmed the approximated solution into the animation algorithm. The
+                final result is not too bad, but not accurate. Its worth noting that since a
+                rocket's acceleration increases over time as mass is lost due to burnt and ejected
+                fuel, the velocity increases in a somewhat exponential nature. I do plan to improve
+                what I have here, and I think one of the possible ways to use the initial analytical
+                solution is to first store numerical results in a table, then do a simple linear
+                interpolation according to what is needed.
+              </Typography>
+            </div>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+
+        <Typography variant={'body2'} gutterBottom>
           Key takeaway: fried my brain a little bit
         </Typography>
       </div>
--- src/components/projects/aerospace/Rocket.js
+++ src/components/projects/aerospace/Rocket.js
@@ -3,12 +3,15 @@ import { useScreen } from '../../../custom-hooks/useScreen';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
-import clsx from 'clsx';
 import { Animate } from 'react-move';
 import { ReactComponent as RocketSVG } from './rocket.svg';
+import clsx from 'clsx';
 
 const useStyles = makeStyles(theme => ({
-  root: {},
+  root: {
+    position: 'relative',
+    zIndex: 2000,
+  },
   launchPlatform: {
     width: 120,
     textAlign: 'center',
