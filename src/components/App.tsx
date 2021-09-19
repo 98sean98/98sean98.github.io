48048617b3879dd07ba4c1409fc03ffe8030e0c8
Sean Chok <seanchok@gmail.com>
2021-09-20 00:35:22 +0800

    make quick rebuild (#57)
    * add profile picture
    
    * add some details

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,27 +1,42 @@
 import React from 'react';
 
-import { Coffee } from '@emotion-icons/feather';
-
-import colors from '../theme/colors';
+import ProfilePicture from '../assets/images/profile_pic.png';
 
 function App() {
   return (
-    <div
-      className={
-        'h-screen flex flex-col justify-center items-center px-4 md:px-8 py-8'
-      }>
-      <div className={'space-y-2'}>
-        <Coffee size={32} color={colors.brand['500']} />
-        <p>Hi, my website is undergoing some major updates.</p>
-        <p>If you want to check out my stuff, you can find them here:</p>
-        <ul className={'list-disc list-inside'}>
-          <li>
-            <a href={'https://www.linkedin.com/in/98sean98'}>linkedin</a>
-          </li>
-          <li>
-            <a href={'https://www.github.com/98sean98'}>github</a>
-          </li>
-        </ul>
+    <div className="m-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+      <div className="col-span-1 flex flex-col justify-center items-center">
+        <div className="w-full" style={{ maxWidth: '12rem' }}>
+          <div className="relative w-full" style={{ paddingBottom: '100%' }}>
+            <div className="absolute inset-0 rounded-md overflow-hidden">
+              <img
+                src={ProfilePicture}
+                alt="profile"
+                className="w-full h-full object-cover"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-span-1 md:col-span-2 lg:col-span-3">
+        <h1>Hi, I'm Sean Chok.</h1>
+        <p>
+          I'm a mechanical engineering student at the University of Hong Kong,
+          and I'm involved in several robotics and software projects.
+        </p>
+
+        <div className="mt-4">
+          <p>If you want to check out my stuff, you can find them here:</p>
+
+          <ul className="list-disc list-inside">
+            <li>
+              <a href={'https://www.linkedin.com/in/98sean98'}>linkedin</a>
+            </li>
+            <li>
+              <a href={'https://www.github.com/98sean98'}>github</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
