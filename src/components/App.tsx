9c15a87fdd6302b88d8b0e120263704effa15ebb
seanchok <seanchok@gmail.com>
2021-09-20 00:32:09 +0800

    add some details

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,27 +1,12 @@
 import React from 'react';
 
-import { Coffee } from '@emotion-icons/feather';
-
-import colors from '../theme/colors';
-
 import ProfilePicture from '../assets/images/profile_pic.png';
-
-const links = (
-  <>
-    <li>
-      <a href={'https://www.linkedin.com/in/98sean98'}>linkedin</a>
-    </li>
-    <li>
-      <a href={'https://www.github.com/98sean98'}>github</a>
-    </li>
-  </>
-);
 
 function App() {
   return (
-    <div className="min-h-screen m-8 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 lg:gap-8">
-      <div className="col-span-1">
-        <div className="mx-auto" style={{ maxWidth: '12rem' }}>
+    <div className="m-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+      <div className="col-span-1 flex flex-col justify-center items-center">
+        <div className="w-full" style={{ maxWidth: '12rem' }}>
           <div className="relative w-full" style={{ paddingBottom: '100%' }}>
             <div className="absolute inset-0 rounded-md overflow-hidden">
               <img
@@ -33,7 +18,26 @@ function App() {
           </div>
         </div>
       </div>
-      <div className="col-span-1 md:col-span-2 lg:col-span-3"></div>
+      <div className="col-span-1 md:col-span-2 lg:col-span-3">
+        <h1>Hi, I'm Sean Chok.</h1>
+        <p>
+          I'm a mechanical engineering student at the University of Hong Kong,
+          and I'm involved in several robotics and software projects.
+        </p>
+
+        <div className="mt-4">
+          <p>If you want to check out my stuff, you can find them here:</p>
+
+          <ul className="list-disc list-inside">
+            <li>
+              <a href={'https://www.linkedin.com/in/98sean98'}>linkedin</a>
+            </li>
+            <li>
+              <a href={'https://www.github.com/98sean98'}>github</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
