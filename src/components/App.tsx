c428de0d5f517bd360820f4d415166a8c2e69507
seanchok <seanchok@gmail.com>
2021-09-25 14:11:08 +0800

    minor ui fixes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,8 +4,8 @@ import ProfilePicture from '../assets/images/profile_pic.png';
 
 function App() {
   return (
-    <div className="m-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-      <div className="col-span-1 flex flex-col justify-center items-center">
+    <div className="m-8 grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-8">
+      <div className="col-span-1 flex justify-center">
         <div className="w-full" style={{ maxWidth: '12rem' }}>
           <div className="relative w-full" style={{ paddingBottom: '100%' }}>
             <div className="absolute inset-0 rounded-md overflow-hidden">
@@ -18,7 +18,7 @@ function App() {
           </div>
         </div>
       </div>
-      <div className="col-span-1 md:col-span-2 lg:col-span-3">
+      <div className="col-span-1 sm:col-span-2 lg:col-span-3">
         <h1>Hi, I'm Sean Chok.</h1>
         <p>
           I'm a mechanical engineering student at the University of Hong Kong,
